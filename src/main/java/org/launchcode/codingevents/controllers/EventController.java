d5a02b0c92d47afe01b8efd33d0fa006fe978f4b
elizabethtarantola <liztarantola@gmail.com>
2021-05-27 12:03:25 -0500

    initial commit

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -16,8 +17,17 @@ public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
     @GetMapping
     public String displayAllEvents(Model model) {
+        HashMap<String,String> events = new HashMap<>();
+        events.put("Strange Loop", "Thurs, Sept 30, 9AM-Sat, Oct 2, 6PM  *Conference for web developers");
+        events.put("4 Weeks SQL Training Course", "Mon, Jun 21, 8:30AM-July 21  *4 week Python Programming Training Course");
+        events.put("4 Weekends Ruby on Rails for Beginners", "Sat Jun 5, 10:30AM-12:30PM *4Weekend Java for beginners");
         model.addAttribute("events", events);
         return "events/index";
     }
